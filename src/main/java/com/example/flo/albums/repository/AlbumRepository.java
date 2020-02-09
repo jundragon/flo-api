7a6d2b56fa11590e1c414a4d11fe60534d6847cf
jundragon <jjundragon88@gmail.com>
2020-02-09 19:45:13 +0900

    repository test

---
+++ src/main/java/com/example/flo/albums/repository/AlbumRepository.java
@@ -0,0 +1,7 @@
+package com.example.flo.albums.repository;
+
+import com.example.flo.albums.domain.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+}
---
+++ src/test/java/com/example/flo/albums/repository/AlbumRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.example.flo.albums.repository;
+
+import com.example.flo.albums.domain.Album;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class AlbumRepositoryTest {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Before
+    public void 테스트데이터생성() {
+        albumRepository.save(new Album("앨범1"));
+        albumRepository.save(new Album("앨범2"));
+        albumRepository.save(new Album("앨범3"));
+        albumRepository.save(new Album("앨범4"));
+        albumRepository.save(new Album("앨범5"));
+        albumRepository.save(new Album("앨범6"));
+        albumRepository.save(new Album("앨범7"));
+        albumRepository.save(new Album("앨범8"));
+        albumRepository.save(new Album("앨범9"));
+        albumRepository.save(new Album("앨범10"));
+    }
+
+    @Test
+    public void 기본테스트() throws Exception {
+        Album album = new Album("앨범 1");
+        albumRepository.save(album);
+
+        Album findAlbum = albumRepository.findById(album.getId()).get();
+        assertThat(findAlbum).isEqualTo(album);
+    }
+
+    @Test
+    public void 페이징테스트() throws Exception {
+        // given
+        PageRequest pageRequest = PageRequest.of(0, 3);
+
+        // when
+        Page<Album> page = albumRepository.findAll(pageRequest);
+
+        // then
+        List<Album> content = page.getContent();
+        long totalElements = page.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(10);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(4);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
+
+    }
+}
